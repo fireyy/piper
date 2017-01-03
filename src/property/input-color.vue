ecc50c92c8aa890954be8cf30c4d9d6bcda2dd4c
fireyy <fireyy@gmail.com>
2017-01-03 17:00:12 +0800

    color picker

--- src/property/input-color.vue
+++ src/property/input-color.vue
@@ -1,29 +1,42 @@
 <template>
   <el-form-item :label="data.title || title" class="input-color">
-    <input type="color" v-model="data.value">
+    <!-- <input type="color" v-model="data.value"> -->
+    <el-popover
+            ref="popover2"
+            class="color-popover"
+            trigger="click">
+        <color-picker v-model="data.value" @change-color="onChange"></color-picker>
+    </el-popover>
+    <span :style="{background:data.value}" class="color" v-popover:popover2></span>
   </el-form-item>
 </template>
 
 <script>
+import { Sketch } from 'vue-color'
 export default {
+  components: {'color-picker': Sketch},
   props: {
     data: {
       type: Object
     },
     title: String,
     index: [String, Number]
+  },
+  methods: {
+    onChange(val){
+      this.data.value = `rgba(${val.rgba.r},${val.rgba.g},${val.rgba.b},${val.rgba.a})`
+    }
   }
 }
 </script>
 
 <style lang="less">
 .input-color {
-  input {
-    padding: 0;
-    border-width: 1px;
-    border-style: solid;
-    border-color: rgb(169, 169, 169);
-    border-image: initial;
+  .color {
+    width: 30px;
+    height: 30px;
+    display: inline-block;
+    border-radius: 5px;
   }
 }
 </style>
