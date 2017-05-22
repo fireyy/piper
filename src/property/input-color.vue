3f068694ffc5b29fe0c340afaec54320aa13f0d7
fireyy <fireyy@gmail.com>
2017-05-22 17:41:59 +0800

    feat: use el-color-picker

--- src/property/input-color.vue
+++ src/property/input-color.vue
@@ -1,51 +1,12 @@
 <template>
   <el-form-item :label="label" class="input-color">
-    <!-- <input type="color" v-model="data.value"> -->
-    <el-popover
-            ref="popover2"
-            class="color-popover"
-            trigger="click">
-        <color-picker v-model="mColor" @change-color="onChange"></color-picker>
-    </el-popover>
-    <span :style="{background:data.value}" class="color" v-popover:popover2></span>
+    <el-color-picker v-model="data.value" show-alpha></el-color-picker>
   </el-form-item>
 </template>
 
 <script>
 import common from './common'
-import { Sketch } from 'vue-color'
-import tinycolor from 'tinycolor2'
 export default {
-  mixins: [common],
-  components: {'color-picker': Sketch},
-  computed: {
-    mColor(){
-      let color = tinycolor(this.data.value)
-      return {
-        hex: color.toHexString(),
-        rgba: color.toRgb(),
-        a: color.getAlpha()
-      }
-    }
-  },
-  methods: {
-    onChange(val){
-      let rgba = val.rgba
-      this.data.value = `rgba(${[rgba.r, rgba.g, rgba.b, rgba.a].join(',')})`
-    }
-  }
+  mixins: [common]
 }
 </script>
-
-<style lang="less">
-.input-color {
-  .color {
-    width: 24px;
-    height: 24px;
-    display: inline-block;
-    vertical-align: middle;
-    border-radius: 3px;
-    box-shadow: inset 0 0 0 1px rgba(0,0,0,.15), inset 0 0 4px rgba(0,0,0,.25);
-  }
-}
-</style>
