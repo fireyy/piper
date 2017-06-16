c1dc5a3147fd35e60f1d146187d6db05ec8a4493
fireyy <fireyy@gmail.com>
2017-06-16 16:34:55 +0800

    fix: remove glob

--- server/lib/api.js
+++ server/lib/api.js
@@ -1,6 +1,5 @@
 const KoaRouter = require('koa-router');
 const apiRouter = new KoaRouter({ prefix: '/api' });
-const glob = require('glob');
 const fs = require("fs");
 const path = require('path');
 
@@ -22,17 +21,6 @@ fs
       }
     }
   });
-
-// glob.sync(path.join(__dirname, '../controllers/*.js')).forEach(dep => {
-//   let controller = require(dep);
-//   for (let method of [ 'options', 'get', 'post', 'delete', 'put' ]) {
-//     if (method in controller) {
-//       apiRouter[method](controller.url, async (ctx, next) => {
-//         return controller[method](ctx).then(next);
-//       });
-//     }
-//   }
-// });
 
 apiRouter.get('/logout', (ctx) => {
   ctx.logout()
